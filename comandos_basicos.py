a79d242f114d3adf215df2d7e16a70a0d6358750
leoraspante <leonardo.raspante@gmail.com>
2025-09-09 10:07:14 -0300

    Adiciona arquivos de conceitos básicos em Python.

---
+++ comandos_basicos.py
@@ -0,0 +1,44 @@
+
+# Declaração de variáveis.
+nome = "Leonardo"
+idade = 32
+altura = 1.73
+
+# Print concatenando variáveis.
+print(f"{nome} possui {idade} anos, e uma altura de {altura} metros.")
+
+distancia = 5568.78996585
+
+# Print formatando casas decimais através do :.2f.
+print(f"{nome} caminha diariamente uma distância de {distancia:.2f} metros.")
+print(" ")
+print(" ")
+
+
+# Primeiro exercício - Declarar variáveis e exibir um print conforme solicitado.
+
+product1 = "Computer"
+product2 = "Office desk"
+
+age = 30
+code = 5290
+gender = "F"
+
+price1 = 2100.0
+price2 = 650.50
+measure = 53.234567
+
+print("Products:")
+
+print(f"{product1}, which price is $ {price1:.2f}\n{product2}, which price is $ {price2:.2f}")
+print(" ")
+
+print(f"Record: {age} years old, code {code} and gender: {gender}")
+print(" ")
+
+print(f"Measue with eight decimal places: {measure:.8f}")
+print(f"Rouded (three decimal places): {measure:.3f}")
+print(f"US decimal point: {measure:.3f}")
+
+
+
